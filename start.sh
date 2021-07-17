86882d5b40de3302ec484651a0f544fd3603878e
kumavis <aaron@kumavis.me>
2021-07-17 13:53:38 -0700

    go

--- start.sh
+++ start.sh
@@ -7,17 +7,17 @@ set -u
 # install deps
 npm install -g concurrently
 
-# # download repos
-# mkdir -p ./repos
-# cd ./repos
-# concurrently \
-#   "git clone git@github.com:MetaMask/metamask-extension.git" \
-#   "git clone git@github.com:MetaMask/metamask-mobile.git" \
-#   "git clone git@github.com:MetaMask/metamask-docs.git" \
-#   "git clone git@github.com:MetaMask/KeyringController.git" \
-#   "git clone git@github.com:MetaMask/eth-sig-util.git" \
-#   "git clone git@github.com:MetaMask/eth-json-rpc-middleware.git"
-# cd ..
+# download repos
+mkdir -p ./repos
+cd ./repos
+concurrently \
+  "git clone git@github.com:MetaMask/metamask-extension.git" \
+  "git clone git@github.com:MetaMask/metamask-mobile.git" \
+  "git clone git@github.com:MetaMask/metamask-docs.git" \
+  "git clone git@github.com:MetaMask/KeyringController.git" \
+  "git clone git@github.com:MetaMask/eth-sig-util.git" \
+  "git clone git@github.com:MetaMask/eth-json-rpc-middleware.git"
+cd ..
 
 # gource
 START_DATE="2021-01-01" END_DATE="2021-07-01" \
