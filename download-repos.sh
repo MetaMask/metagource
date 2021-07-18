404d8d3c91e063836ad3bfc053de87df25c13b79
Dan Finlay <dan@danfinlay.com>
2021-07-18 12:13:49 -0700

    Add more relevant repos

--- download-repos.sh
+++ download-repos.sh
@@ -11,7 +11,13 @@ concurrently \
   "git clone git@github.com:MetaMask/metamask-extension.git" \
   "git clone git@github.com:MetaMask/metamask-mobile.git" \
   "git clone git@github.com:MetaMask/metamask-docs.git" \
+  "git clone git@github.com:MetaMask/controllers.git" \
   "git clone git@github.com:MetaMask/KeyringController.git" \
+  "git clone git@github.com:MetaMask/eth-simple-keyring.git" \
+  "git clone git@github.com:MetaMask/eth-hd-keyring.git" \
+  "git clone git@github.com:MetaMask/eth-trezor-keyring.git" \
+  "git clone git@github.com:MetaMask/eth-ledger-bridge-keyring.git" \
   "git clone git@github.com:MetaMask/eth-sig-util.git" \
   "git clone git@github.com:MetaMask/eth-json-rpc-middleware.git"
+  "git clone git@github.com:MetaMask/rpc-cap.git" \
 cd ..
